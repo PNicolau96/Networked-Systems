af4b8c54e073678edcf40a27489cd191755b7002
PNicolau96 <55545405+PNicolau96@users.noreply.github.com>
2021-11-22 17:30:58 +0100

    Part 2 Blocking Probabilities also done

--- SoftwareProgram.py
+++ SoftwareProgram.py
@@ -1,6 +1,8 @@
 import math
 import matplotlib.pyplot as plt
 
+###################################
+#PART1
 
 # Defining the calculation of the block probability:
 
@@ -8,7 +10,7 @@ def BlockingProb(lamb=15/60,beta=4,N=5):
     
     numerator = (lamb*beta)**N / math.factorial(N)
 
-    denominator = 1 
+    denominator = 1
 
     for i in range(1,N+1):
         denominator = denominator + (lamb*beta)**i / math.factorial(i)
@@ -53,9 +55,75 @@ def Plot_Ncores(lamb=15/60, beta=4, Nmax=10):
     plt.show()
 
 
-Plot_Ncards()
-Plot_Ncores()
+#Plot_Ncards()
+#Plot_Ncores()
+#BlockingProb()
 
 #At 7 cores we're at a BP of 0.0073%
 
+###################################
+#PART2
 
+
+    
+def KaufmanRoberts(C,K,b,rho):
+    '''
+    C : number of available channels
+    K : number of job types
+    b : list of required capacities for each k
+    rho : list of loads for each k
+    '''
+    g = [0]*(C+1) #List of each g(c)
+    
+    for c in range(C+1):
+
+        if c==0:
+            g[c]=1
+
+        else:
+
+            for k in range(K):
+
+                if c-b[k]==0:
+                    d = 1
+                elif c-b[k]<-1:
+                    d = 0
+                else:
+                    d = g[c-b[k]]
+            
+                g[c] += b[k]*rho[k]*d
+            g[c] = (1/c)*g[c]
+            #print(f"g({c}) is {g[c]}")
+
+    G = sum(g)
+    #print(f"G is {G}")
+
+
+    q = []
+    for c in range(C+1):
+        q.append(g[c]/G)
+        #print(f"q({c}) is {q[c]}")
+
+
+    B = [0]*K
+    for k in range(K):
+        for g in range(K):
+            if C-b[k]+g > 4:
+                continue 
+            B[k] += q[(C-b[k]+g)]
+        print(f"B({k}) is {round(B[k]*100,2)}%")
+
+    return g,q,B
+
+
+lamb = [0.25, 0.007*0.7, 0.002*0.2, 0.001*0.1]
+mu = [1/4, 1/20, 1/20, 1/20]
+b=[1,3,4,5]
+rho = []
+for i in range(len(lamb)):
+    rho.append(lamb[i]/mu[i])
+
+
+g,q,B = KaufmanRoberts(5,4,b,rho)
+
+#print(rho,g,q,B)
