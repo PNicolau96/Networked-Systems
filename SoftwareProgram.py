359c9da4f91b5b17d145fd13013c907296c735f9
PNicolau96 <55545405+PNicolau96@users.noreply.github.com>
2021-11-25 21:52:48 +0100

    final push

--- SoftwareProgram.py
+++ SoftwareProgram.py
@@ -156,75 +156,104 @@ cond3 = 7/38
 cond4 = 11/38
 cond5 = 16/38
 
-# Mobile Voice Networks Cells
-for CellChansOne in range(1,Channels+1):
-    channels_one = CellChansOne #number of channels in cell 1
-    for CellChansTwo in range(1,(Channels+1)-channels_one):
-        channels_two = CellChansTwo #number of channels in cell 2
-        for CellChansThree in range(1,(Channels+1)-channels_one-channels_two):
-            channels_three = CellChansThree # number of channels in cell 3
-            for CellChansFour in range(1,(Channels+1)-channels_one-channels_two-channels_three):
-                channels_four = CellChansFour # number of channels in cell 4
-                channels_five = Channels - channels_one - channels_two - channels_three - channels_four
-                if channels_five ==0:
-                    continue
+def CellsNetwork():
 
-                ChannelDistribution.append([channels_one,channels_two,channels_three,channels_four,channels_five])
+    lambs = [1,3,7,11,16]
 
-                #combination 1
-                BPAone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four+channels_five)
-                BPAtwo = cond2*BlockingProb(lambs[1],1.5,channels_two)
-                BPAthree = cond3*BlockingProb(lambs[2],1.5,channels_three)
-                BPAfour = cond4*BlockingProb(lambs[3],1.5,channels_four+channels_one)
-                BPAfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_two)
-                BPA = BPAone*BPAtwo*BPAthree*BPAfour*BPAfive
-                #combination 2
-                BPBone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four)
-                BPBtwo = cond2*BlockingProb(lambs[1],1.5,channels_two+channels_five)
-                BPBthree = BPAthree
-                BPBfour = BPAfour
-                BPBfive = BPAfive
-                BPB = BPBone*BPBtwo*BPBthree*BPBfour*BPBfive
-                #combination 3
-                BPCone = BPBone
-                BPCtwo = BPBtwo
-                BPCthree = BPAthree
-                BPCfour = cond4*BlockingProb(lambs[3],1.5,channels_four)
-                BPCfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_one+channels_two)
-                BPC = BPCone*BPCtwo*BPCthree*BPCfour*BPCfive
-                #combination 4
-                BPDone = BPAone
-                BPDtwo = BPAtwo
-                BPDthree = BPAthree
-                BPDfour = BPCfour
-                BPDfive = BPCfive
-                BPD = BPDone*BPDtwo*BPDthree*BPDfour*BPDfive
+    OverallBPlist = []
+    BPperBestCell = []
+    ChannelDistribution = []
+    CombinationTracker = []
 
-                BestCombination = min(BPA,BPB,BPC,BPD) #Find which of the combinations yield the lowest overall blocking probability
-                OverallBPlist.append(BestCombination)
+    cond1 = 1/38
+    cond2 = 3/38
+    cond3 = 7/38
+    cond4 = 11/38
+    cond5 = 16/38
 
-                #Save the blocking probabilities of the best overall combination and that of each of its cells
-                if BPA==BestCombination:
-                    CombinationTracker.append(1)
-                    BPperBestCell.append([BPAone,BPAtwo,BPAthree,BPAfour,BPAfive])
-                elif BPB==BestCombination:
-                    CombinationTracker.append(2)
-                    BPperBestCell.append([BPBone,BPBtwo,BPBthree,BPBfour,BPBfive])
-                elif BPC==BestCombination:
-                    CombinationTracker.append(3)
-                    BPperBestCell.append([BPCone,BPCtwo,BPCthree,BPCfour,BPCfive])
-                elif BPD==BestCombination:
-                    CombinationTracker.append(4)
-                    BPperBestCell.append([BPDone,BPDtwo,BPDthree,BPDfour,BPDfive])
+    breakout=False
 
+    for Channels in range(60,151):
+        # Mobile Voice Networks Cells
+        for CellChansOne in range(1,Channels+1):
+            if breakout:
+                break
+            channels_one = CellChansOne #number of channels in cell 1
+            for CellChansTwo in range(1,(Channels+1)-channels_one):
+                if breakout:
+                    break
+                channels_two = CellChansTwo #number of channels in cell 2
+                for CellChansThree in range(1,(Channels+1)-channels_one-channels_two):
+                    if breakout:
+                        break
+                    channels_three = CellChansThree # number of channels in cell 3
+                    for CellChansFour in range(1,(Channels+1)-channels_one-channels_two-channels_three):
+                        channels_four = CellChansFour # number of channels in cell 4
+                        channels_five = Channels - channels_one - channels_two - channels_three - channels_four
+                        if channels_five ==0:
+                            continue
 
-min_overallBP = min(OverallBPlist)
-BPindex = OverallBPlist.index(min_overallBP)
+                        ChannelDistribution.append([channels_one,channels_two,channels_three,channels_four,channels_five])
 
-Solution = [min_overallBP,ChannelDistribution[BPindex],CombinationTracker[BPindex],BPperBestCell[BPindex]]
-print(Solution)
+                        #combination 1
+                        BPAone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four+channels_five)
+                        BPAtwo = cond2*BlockingProb(lambs[1],1.5,channels_two)
+                        BPAthree = cond3*BlockingProb(lambs[2],1.5,channels_three)
+                        BPAfour = cond4*BlockingProb(lambs[3],1.5,channels_four+channels_one)
+                        BPAfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_two)
+                        BPA = 1-((1-BPAone)*(1-BPAtwo)*(1-BPAthree)*(1-BPAfour)*(1-BPAfive))
+                        #combination 2
+                        BPBone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four)
+                        BPBtwo = cond2*BlockingProb(lambs[1],1.5,channels_two+channels_five)
+                        BPBthree = BPAthree
+                        BPBfour = BPAfour
+                        BPBfive = BPAfive
+                        BPB = 1-((1-BPBone)*(1-BPBtwo)*(1-BPBthree)*(1-BPBfour)*(1-BPBfive))
+                        #combination 3
+                        BPCone = BPBone
+                        BPCtwo = BPBtwo
+                        BPCthree = BPAthree
+                        BPCfour = cond4*BlockingProb(lambs[3],1.5,channels_four)
+                        BPCfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_one+channels_two)
+                        BPC = 1-((1-BPCone)*(1-BPCtwo)*(1-BPCthree)*(1-BPCfour)*(1-BPCfive))
+                        #combination 4
+                        BPDone = BPAone
+                        BPDtwo = BPAtwo
+                        BPDthree = BPAthree
+                        BPDfour = BPCfour
+                        BPDfive = BPCfive
+                        BPD = 1-((1-BPDone)*(1-BPDtwo)*(1-BPDthree)*(1-BPDfour)*(1-BPDfive))
 
-end = timeit.timeit()
-print(end - start)
+                        BestCombination = min(BPA,BPB,BPC,BPD) #Find which of the combinations yield the lowest overall blocking probability
+                        OverallBPlist.append(BestCombination)
+
+                        #Save the blocking probabilities of the best overall combination and that of each of its cells
+                        if BPA==BestCombination:
+                            CombinationTracker.append(1)
+                            BPperBestCell.append([BPAone,BPAtwo,BPAthree,BPAfour,BPAfive])
+                        elif BPB==BestCombination:
+                            CombinationTracker.append(2)
+                            BPperBestCell.append([BPBone,BPBtwo,BPBthree,BPBfour,BPBfive])
+                        elif BPC==BestCombination:
+                            CombinationTracker.append(3)
+                            BPperBestCell.append([BPCone,BPCtwo,BPCthree,BPCfour,BPCfive])
+                        elif BPD==BestCombination:
+                            CombinationTracker.append(4)
+                            BPperBestCell.append([BPDone,BPDtwo,BPDthree,BPDfour,BPDfive])
+
+                        if BestCombination<0.025:
+                            breakout = True
+                            break
+
+        min_overallBP = min(OverallBPlist)
+        BPindex = OverallBPlist.index(min_overallBP)
+
+        Solution = [min_overallBP,ChannelDistribution[BPindex],CombinationTracker[BPindex],BPperBestCell[BPindex]]
+        print(Solution)
+
+        end = timeit.timeit()
+        print(end - start)
+
+CellsNetwork()
 
 #print(len(OverallBPlist),len(ChannelDistribution),len(CombinationTracker),len(BPperBestCell))
