a897662cb119d1e7aaab617a22ecee5eaf90823f
PNicolau96 <55545405+PNicolau96@users.noreply.github.com>
2021-11-25 18:58:48 +0100

    part 3

--- SoftwareProgram.py
+++ SoftwareProgram.py
@@ -1,6 +1,11 @@
 import math
 import matplotlib.pyplot as plt
 import sys
+import timeit
+
+start = timeit.timeit()
+print("hello")
+
 
 ###################################
 #PART1
@@ -18,30 +23,31 @@ def BlockingProb(lamb=15/60,beta=4,N=5):
 
     BP = numerator/denominator 
 
-    print('The blocking probability of the', N, 'core system, with a rate of', lamb*60, 'jobs per hour and an average job duration of', beta, 'minutes is', round(BP*100, 5), '%')
     return BP 
 
 
 # Plotting Blocking Probability for increasing number of video cards:
 
-def Plot_Ncards(lamb=15/60, beta=4, N=5, Nmax=10):
+def Plot_Ncards(lamb=15/60, beta=4, N=5, Nmax=5):
 
     BPlist=[]
 
     for n in range(1,Nmax+1):
         BP = BlockingProb(lamb,beta,n*N)
         BPlist.append(BP)
-    
+    print([element * 100 for element in BPlist])
     plt.plot(range(1,Nmax+1),[element * 100 for element in BPlist])
     plt.ylabel('Blocking Probability (%)')
     plt.xlabel('# of Video Cards')
     plt.title('Blocking Probability for increasing # of Video Cards (5 cores each)')
+    plt.grid(which='major', axis='both')
+    plt.xticks(range(1, 6))
     plt.show()
 
 
 # Plotting Blocking Probability for increasing number of individual CPUs:
 
-def Plot_Ncores(lamb=15/60, beta=4, Nmax=10):
+def Plot_Ncores(lamb=15/60, beta=4, Nmax=8):
 
     BPlist=[]
 
@@ -49,10 +55,12 @@ def Plot_Ncores(lamb=15/60, beta=4, Nmax=10):
         BP = BlockingProb(lamb,beta,N)
         BPlist.append(BP)
     
+    print([element * 100 for element in BPlist])
     plt.plot(range(1,Nmax+1),[element * 100 for element in BPlist])
     plt.ylabel('Blocking Probability (%)')
     plt.xlabel('# of CPUs')
     plt.title('Blocking Probability for increasing # of CPUs')
+    plt.grid(which='major', axis='both')
     plt.show()
 
 
@@ -80,6 +88,7 @@ def KaufmanRoberts(C,K,b,rho):
 
         if c==0:
             g[c]=1
+            print(f"g({c}) is {g[c]}")
 
         else:
 
@@ -94,16 +103,16 @@ def KaufmanRoberts(C,K,b,rho):
             
                 g[c] += b[k]*rho[k]*d
             g[c] = (1/c)*g[c]
-            #print(f"g({c}) is {g[c]}")
+            print(f"g({c}) is {g[c]}")
 
     G = sum(g)
-    #print(f"G is {G}")
+    print(f"G is {G}")
 
 
     q = []
     for c in range(C+1):
         q.append(g[c]/G)
-        #print(f"q({c}) is {q[c]}")
+        print(f"q({c}) is {q[c]}")
 
     B = [0]*K
     for k in range(K):
@@ -123,24 +132,31 @@ for i in range(len(lamb)):
     rho.append(lamb[i]/mu[i])
 
 
-g,q,B = KaufmanRoberts(5,4,b,rho)
+#g,q,B = KaufmanRoberts(5,4,b,rho)
 
-#print(rho,g,q,B)
 
 ###################################
 #PART3
 
-file_path = 'out.txt'
-sys.stdout = open(file_path, "w") #for printing to a .txt file
+#file_path = 'out.txt'
+#sys.stdout = open(file_path, "w") #for printing to a .txt file
 
 lambs = [1,3,7,11,16]
 betas = [1.5]*5
 Cells=5
 Channels=60
+OverallBPlist = []
+BPperBestCell = []
+ChannelDistribution = []
+CombinationTracker = []
 
+cond1 = 1/38
+cond2 = 3/38
+cond3 = 7/38
+cond4 = 11/38
+cond5 = 16/38
 
 # Mobile Voice Networks Cells
-
 for CellChansOne in range(1,Channels+1):
     channels_one = CellChansOne #number of channels in cell 1
     for CellChansTwo in range(1,(Channels+1)-channels_one):
@@ -149,13 +165,66 @@ for CellChansOne in range(1,Channels+1):
             channels_three = CellChansThree # number of channels in cell 3
             for CellChansFour in range(1,(Channels+1)-channels_one-channels_two-channels_three):
                 channels_four = CellChansFour # number of channels in cell 4
-                channels_five = Channels - channels_one -channels_two - channels_three - channels_four
+                channels_five = Channels - channels_one - channels_two - channels_three - channels_four
                 if channels_five ==0:
                     continue
-                #print(channels_one, channels_two, channels_three, channels_four, channels_five)
-                
-                
-                        
 
-         
-                
+                ChannelDistribution.append([channels_one,channels_two,channels_three,channels_four,channels_five])
+
+                #combination 1
+                BPAone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four+channels_five)
+                BPAtwo = cond2*BlockingProb(lambs[1],1.5,channels_two)
+                BPAthree = cond3*BlockingProb(lambs[2],1.5,channels_three)
+                BPAfour = cond4*BlockingProb(lambs[3],1.5,channels_four+channels_one)
+                BPAfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_two)
+                BPA = BPAone*BPAtwo*BPAthree*BPAfour*BPAfive
+                #combination 2
+                BPBone = cond1*BlockingProb(lambs[0],1.5,channels_one+channels_four)
+                BPBtwo = cond2*BlockingProb(lambs[1],1.5,channels_two+channels_five)
+                BPBthree = BPAthree
+                BPBfour = BPAfour
+                BPBfive = BPAfive
+                BPB = BPBone*BPBtwo*BPBthree*BPBfour*BPBfive
+                #combination 3
+                BPCone = BPBone
+                BPCtwo = BPBtwo
+                BPCthree = BPAthree
+                BPCfour = cond4*BlockingProb(lambs[3],1.5,channels_four)
+                BPCfive = cond5*BlockingProb(lambs[4],1.5,channels_five+channels_one+channels_two)
+                BPC = BPCone*BPCtwo*BPCthree*BPCfour*BPCfive
+                #combination 4
+                BPDone = BPAone
+                BPDtwo = BPAtwo
+                BPDthree = BPAthree
+                BPDfour = BPCfour
+                BPDfive = BPCfive
+                BPD = BPDone*BPDtwo*BPDthree*BPDfour*BPDfive
+
+                BestCombination = min(BPA,BPB,BPC,BPD) #Find which of the combinations yield the lowest overall blocking probability
+                OverallBPlist.append(BestCombination)
+
+                #Save the blocking probabilities of the best overall combination and that of each of its cells
+                if BPA==BestCombination:
+                    CombinationTracker.append(1)
+                    BPperBestCell.append([BPAone,BPAtwo,BPAthree,BPAfour,BPAfive])
+                elif BPB==BestCombination:
+                    CombinationTracker.append(2)
+                    BPperBestCell.append([BPBone,BPBtwo,BPBthree,BPBfour,BPBfive])
+                elif BPC==BestCombination:
+                    CombinationTracker.append(3)
+                    BPperBestCell.append([BPCone,BPCtwo,BPCthree,BPCfour,BPCfive])
+                elif BPD==BestCombination:
+                    CombinationTracker.append(4)
+                    BPperBestCell.append([BPDone,BPDtwo,BPDthree,BPDfour,BPDfive])
+
+
+min_overallBP = min(OverallBPlist)
+BPindex = OverallBPlist.index(min_overallBP)
+
+Solution = [min_overallBP,ChannelDistribution[BPindex],CombinationTracker[BPindex],BPperBestCell[BPindex]]
+print(Solution)
+
+end = timeit.timeit()
+print(end - start)
+
+#print(len(OverallBPlist),len(ChannelDistribution),len(CombinationTracker),len(BPperBestCell))
